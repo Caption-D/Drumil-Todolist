62019b74222adbc10e85be890a554f8a4c3c7a1c
Caption-D <91252875+Caption-D@users.noreply.github.com>
2022-09-25 18:22:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function showTask() {
     addedTask.push(taskInput);
 
     // showTask.innerHTML += `<li style="list-style-type:none" class="" id="${++i}"><input class="" type="checkbox" onclick="crossTask()" id="${++i}" > ${taskInput} </li>`;
-    showTask.innerHTML += `<li style="list-style-type:none" class="" onclick="this.classList.toggle('strikeTHrough')" id="${i++}"> ${taskInput} </li>`;
+    showTask.innerHTML += `<li style="list-style-type:none" class="liItems" onclick="this.classList.toggle('strikeTHrough')" id="${i++}"> ${taskInput} </li>`;
   // event.classList.toggle('crossOff');
     document.getElementById('tasks').value = '';
     console.log("\n\n"+i);
